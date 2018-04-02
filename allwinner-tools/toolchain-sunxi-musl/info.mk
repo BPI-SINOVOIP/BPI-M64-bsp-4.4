67c9ed36cb2016521958cd0bcad89087ffa4aefd
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2018-04-02 15:46:45 +0800

    add allwinner tina 2.5.1 toolchain

---
+++ allwinner-tools/toolchain-sunxi-musl/info.mk
@@ -0,0 +1,6 @@
+TARGET_CROSS=aarch64-openwrt-linux-musl-
+GCC_VERSION=linaro-5.2-2015.11-2
+LIBC_TYPE=musl
+LIBC_URL=http://www.musl-libc.org/
+LIBC_VERSION=1.1.14
+LIBC_SO_VERSION=1.1.14
---
+++ allwinner-tools/toolchain-sunxi-musl/lib/gcc/aarch64-openwrt-linux-musl/5.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/liumingyuan/workspace/tina-mydev/out/tulip-d1/compile_dir/toolchain/gcc-linaro-5.2-2015.11-2/configure --with-bugurl=https://dev.openwrt.org/ --with-pkgversion='OpenWrt/Linaro GCC 5.2-2015.11-2 2016-11-07' --prefix=/home/liumingyuan/workspace/tina-mydev/out/tulip-d1/staging_dir/toolchain --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=aarch64-openwrt-linux-musl --with-gnu-ld --enable-target-optspace --enable-libgomp --disable-libmudflap --disable-multilib --disable-nls --without-isl --without-cloog --with-host-libstdcxx=-lstdc++ --with-gmp=/home/liumingyuan/workspace/tina-mydev/out/host --with-mpfr=/home/liumingyuan/workspace/tina-mydev/out/host --with-mpc=/home/liumingyuan/workspace/tina-mydev/out/host --disable-decimal-float --with-diagnostics-color=auto-if-env --disable-libssp --enable-fix-cortex-a53-835769 --enable-fix-cortex-a53-843419 --enable-__cxa_atexit --with-headers=/home/liumingyuan/workspace/tina-mydev/out/tulip-d1/staging_dir/toolchain/include --disable-libsanitizer --enable-languages=c,c++ --enable-shared --enable-threads --with-slibdir=/home/liumingyuan/workspace/tina-mydev/out/tulip-d1/staging_dir/toolchain/lib --enable-lto --with-libelf=/home/liumingyuan/workspace/tina-mydev/out/host";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
